da2df5d27b5d18de06305f871239ac7c46622649
PaulGureghian <paulgureghian@att.net>
2020-09-18 15:50:31 -0700

    Added functions

--- AreaCalculator.java
+++ AreaCalculator.java
@@ -30,17 +30,86 @@ public class AreaCalculator {
             int shape = keyboard.nextInt();
             System.out.println();
 
+            keyboard.close();
+
             // Check for user input
-            if(shape == 1) {
-                area_triangle(5,6);
-            
-            } else if(shape == 2) {
-                
+            if (shape == 1) {
+                area_triangle(5, 6);
+
+            } else if (shape == 2) {
+                area_rectangle(4, 5);
+
+            } else if (shape == 3) {
+                area_circle(4);
+
+            } else if (shape == 4) {
+                quit();
+                break;
             }
-
-            } 
         }
+    }
 
-}
+    public static double area_triangle(int base, int height) {
 
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Base: ");
+        base = keyboard.nextInt();
+
+        System.out.println("Height: ");
+        height = keyboard.nextInt();
+
+        System.out.println();
+        keyboard.close();
+
+        int A = (base * height) / 2;
+
+        System.out.println("The area is " + A + ".");
+
+        return A;
+    }
+
+    public static int area_rectangle(int length, int width) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Length: ");
+        length = keyboard.nextInt();
+
+        System.out.println("Width: ");
+        width = keyboard.nextInt();
+
+        System.out.println();
+        keyboard.close();
+
+        int A = length * width;
+
+        System.out.println("The area is " + A + ".");
+
+        return A;
+    }
+
+    public static double area_circle(int radius) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Radius: ");
+        radius = keyboard.nextInt();
+
+        System.out.println();
+        keyboard.close();
+
+        double A = Math.PI * radius * radius;
+
+        System.out.println("The area is " + A + ".");
+
+        return A;
+    }
+
+    public static String quit() {
+
+        System.out.println("Quit program");
+
+        return null;
+    }
 }
